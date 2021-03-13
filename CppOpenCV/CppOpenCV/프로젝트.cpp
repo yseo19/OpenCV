3885f516937d99cbd34c5c6c04fa0525c6054b72
yseo19 <67813798+yseo19@users.noreply.github.com>
2021-03-13 12:57:00 +0900

    Add files via upload

--- CppOpenCV/CppOpenCV/프로젝트.cpp
+++ CppOpenCV/CppOpenCV/프로젝트.cpp
@@ -1,11 +1,6 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
-
-using namespace std;
-using namespace cv;
-
-#include "opencv2/opencv.hpp"
-#include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -22,43 +17,36 @@ void affine_transform() {
 	cout << "FPS : " << fps << endl;
 
 	int delay = cvRound(1000 / fps);
+	int pos = 0;
 
 	Mat frame;
+	vector<Mat> frames;
 
 	while (true) {
 		cap >> frame;
+		frames.push_back(frame);
 		if (frame.empty())
 			break;
 
-
-		/*Point2f srcPts[3], dstPts[3];
-		srcPts[0] = Point2f(0, 0);
-		srcPts[1] = Point2f(frame.cols - 1, 0);
-		srcPts[2] = Point2f(frame.cols - 1, frame.rows - 1);
-		dstPts[0] = Point2f(50, 50);
-		dstPts[1] = Point2f(frame.cols - 100, 100);
-		dstPts[2] = Point2f(frame.cols - 50, frame.rows - 50);
-
-		Mat M = getAffineTransform(srcPts, dstPts);
-
-		Mat dst;
-		warpAffine(frame, dst, M, Size());
-		*/
-		imshow("frame", frame);
-		//imshow("dst", dst);
+		imshow("frames", frames[pos++]);
 
 		int keycode = waitKeyEx(1);
 
 		if (keycode == 0x270000) {
-			for (i = 0; i < fps * 5; i++)
-				cap.grab();
+			for (i = 0; i < fps * 5; i++) {
+				cap >> frame;
+				frames.push_back(frame);
+			}
+			pos += fps * 5;
+				//cap.grab();
 		}
-
+		if (keycode == 0x250000) {
+			pos -= fps * 5;
+			if (pos < 0) pos = 0;
+		}
 		if (keycode == 0x260000) {
 			waitKey();
 		}
-
-
 		if (keycode == 27)
 			break;
 	}
